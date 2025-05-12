0832def536e235985bbc16b30f9b791ca9015d47
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2025-05-12 00:37:26 -0300

    fix: warning on fallthough

--- src/murmur_hash.c
+++ src/murmur_hash.c
@@ -1,11 +1,13 @@
 #include "murmur_hash.h"
-#include <stdint.h> 
+#include <stdint.h>
 
-static inline uint32_t rotl32(uint32_t x, int8_t r) {
+static inline uint32_t rotl32(uint32_t x, int8_t r)
+{
 	return (x << r) | (x >> (32 - r));
 }
 
-static inline uint32_t fmix32(uint32_t h) {
+static inline uint32_t fmix32(uint32_t h)
+{
 	h ^= h >> 16;
 	h *= 0x85ebca6b;
 	h ^= h >> 13;
@@ -14,7 +16,8 @@ static inline uint32_t fmix32(uint32_t h) {
 	return h;
 }
 
-uint32_t murmur_hash(const void *key, int len, uint32_t seed) {
+uint32_t murmur_hash(const void *key, int len, uint32_t seed)
+{
 	const uint8_t *data = (const uint8_t *)key;
 	const int nblocks = len / 4;
 	uint32_t h1 = seed;
@@ -39,8 +42,10 @@ uint32_t murmur_hash(const void *key, int len, uint32_t seed) {
 	{ // len % 4
 	case 3:
 		k1 ^= tail[2] << 16;
+		// fall through
 	case 2:
 		k1 ^= tail[1] << 8;
+		// fall through
 	case 1:
 		k1 ^= tail[0];
 		k1 *= c1;
